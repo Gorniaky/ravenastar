48b26c5906fed5c701eb163368e79fe2ddceef61
RavenaStar <666ravena.js@gmail.com>
2024-02-24 17:04:04 -0300

    up

---
+++ index.js
@@ -0,0 +1,5 @@
+const blocklist = require('./src/blocklist.js')
+
+module.exports = {
+    all: blocklist
+}
---
+++ src/blocklist.js
@@ -0,0 +1,8 @@
+const fetchData = async () => {
+    const axios = require('axios')
+    let url = 'https://raw.githubusercontent.com/ravenastar-js/shortlinks-scams/main/scams/shortlinks.json'
+    const response = await axios.get(url);
+    return response.data
+}
+
+fetchData()
